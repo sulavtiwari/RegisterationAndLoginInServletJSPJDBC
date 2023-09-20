ac75cdbe539e836c0ef5d344238b4962c408355f
sulavtiwari <sulavtiwari55@gmail.com>
2023-09-20 23:32:00 +0545

    added code for retrieving request data (form data)

--- src/main/webapp/Register.java
+++ src/main/webapp/Register.java
@@ -1,4 +1,22 @@
 package main.webapp;
 
-public class Register {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/register")
+public class Register extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String gender = req.getParameter("gender");
+        String age = req.getParameter("age");
+        String telephone = req.getParameter("phone");
+        String password = req.getParameter("password");
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Online Registration for Skyrider College</h2>
-<form method="POST" action="login">
+<form method="POST" action="register">
 Name: <input type="text" name="name"><br>
 Email: <input type="text" name="email"><br>
 Gender: <input type="text" name="gender"><br>
